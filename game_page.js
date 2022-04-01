f70a451f7e3afa1baa100ecfada61ded226ded9b
aarav6 <91319744+aarav6@users.noreply.github.com>
2022-04-01 13:45:55 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -24,7 +24,7 @@ function send() {
 
     question_word="<h4 id='word_display'> Q. "+ remove_charAt3 +" </h4>";
     input_box= "<br>answer : <input type='text' id='input_check_box'>";
-    check_button= "<br><br><button class='btn btn-info' onclick check()>check</button>"
+    check_button= "<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
     row= question_word + input_box + check_button ;
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value="";
@@ -41,6 +41,45 @@ function check()
     console.log("answer in lower case - " + answer);
     if(answer==word)
     {
-        
+       if(answer_turn == "player1")
+       {
+         player1_score= player1_score +1;
+         document.getElementById("player1_score").innerHTML= player1_score;
+       }
+
+       else
+       {
+           player2_score= player2_score+1;
+           document.getElementById("player2_score").innerHTML=player2_score;
+       }
     }
+
+    if(question_turn == "player1")
+    {
+        question_turn= "player2";
+        document.getElementById("player_question").innerHTML= "Question turn - " + player2_name;
+    }
+
+    else
+    {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML= "Question turn - "+ player1_name;
+
+    }
+
+    if(answer_turn == "player1")
+    {
+        answer_turn= "player2";
+        document.getElementById("player_answer").innerHTML= "Answer turn - "+ player2_name;
+    }
+
+    else
+    {
+        answer_turn="player1";
+        document.getElementById("player_answer").innerHTML= "Answer turn - "+ player1_name;
+    }
+
+    document.getElementById("output").innerHTML="";
 }
+
+
